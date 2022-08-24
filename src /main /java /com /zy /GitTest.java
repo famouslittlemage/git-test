2f1b2cfa5d78857c15353c35450bce5530ffa64b
famouslittlemage <111726030+famouslittlemage@users.noreply.github.com>
2022-08-24 13:42:56 +0800

    Update and rename src/main/java/com/zy/GitTest.java to src /main /java /com /zy /GitTest.java

---
+++ src /main /java /com /zy /GitTest.java
@@ -0,0 +1,17 @@
+package com.zy;
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello,git");
+        System.out.println("hello,git push");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+        System.out.println("hello,git pull");
+    }
+}
--- src/main/java/com/zy/GitTest.java
+++
@@ -1,9 +0,0 @@
-package com.zy;
-
-public class GitTest {
-    public static void main(String[] args) {
-        System.out.println("hello,git");
-        System.out.println("hello,git push");
-        System.out.println("hello,git pull");
-    }
-}
